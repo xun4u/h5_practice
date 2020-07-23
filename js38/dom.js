794b145bfb07eb223fcfe95c782c62c87f0845a2
Lex <xunleee@qq.com>
2020-07-23 17:46:10 +0800

    js38简单封装dom的api

---
+++ js38/dom.js
@@ -0,0 +1,18 @@
+window.dom = {
+    find(string) {
+        return document.querySelectorAll(string.trim())
+    },
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            node.style[name] = value
+        } else if (arguments.length === 2) {
+            return node.style[name]
+        }
+    },
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i])
+        }
+    }
+
+}
